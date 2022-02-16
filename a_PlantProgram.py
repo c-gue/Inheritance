471489ae7915676e181e239c616c1101651fda64
Chris <c_guerra@baylor.edu>
2022-02-16 13:21:14 -0600

    wednesday

--- a_PlantProgram.py
+++ a_PlantProgram.py
@@ -2,12 +2,12 @@ import a_PlantClass as pc
 
 primrose = pc.Plant("Green")
 
-sunflower = pc.Flower("Yellow")
+sunflower = pc.Flower("Yellow",12)
 
 print(primrose.get_color())
 
 print(sunflower.get_color())
 print(sunflower.get_petals())
 
-
-print(primrose.get_petals())
+#Plant class does not have access to petals
+#print(primrose.get_petals())
